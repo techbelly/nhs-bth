25726552dfab57e76506d1c473e4c6be9668529c
Ben Griffiths <bengriffiths@gmail.com>
2011-08-16 15:59:03 +0100

    Increased batch size for link downloaded. Added memcache-ing for performance.

--- appengine/api.py
+++ appengine/api.py
@@ -1,5 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.api import memcache
+
 import simplejson
 
 import models
@@ -11,18 +13,22 @@ class link_syncer(webapp.RequestHandler):
         latest_hash = self.request.get("latest",None)
         if not latest_hash:
             link = models.Link.gql("ORDER BY date_added ASC").get()
+            latest_hash = link.key().name()
         else:
             link = models.Link.get_by_key_name(latest_hash)
-            
-        q = models.Link.gql("WHERE date_added > :1 ORDER BY date_added ASC", link.date_added)
-        newer_links = q.fetch(50)
-        def clean(l):
-            return l.key().name()
         
-        as_json = map(clean, newer_links)
+        as_json = memcache.get(latest_hash)
+        if as_json is None:
+            q = models.Link.gql("WHERE date_added > :1 ORDER BY date_added ASC", link.date_added)
+            newer_links = q.fetch(500)
+            def clean(l):
+                return l.key().name()
         
+            as_json = simplejson.dumps(map(clean, newer_links))
+            memcache.add(latest_hash,as_json,30*60)
+
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(simplejson.dumps(as_json))
+        self.response.out.write(as_json)
 
 class article_handler(webapp.RequestHandler):
     def get(self):
